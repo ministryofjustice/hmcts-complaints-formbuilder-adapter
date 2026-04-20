8b9b5bd1d0d8b2110d411c950afa68574bca1b63
Ripan Kumar <er.ripankumar@gmail.com>
2026-04-20 18:41:11 +0100

    # This is a combination of 3 commits. # This is the 1st commit message:
    Fix snyk vunerabilities
    
    # The commit message #2 will be skipped:
    
    # Fix specs
    
    # The commit message #3 will be skipped:
    
    # Fix snyk vunerabilities

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2021_04_28_120407) do
+ActiveRecord::Schema[7.2].define(version: 2021_04_28_120407) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,5 +46,4 @@ ActiveRecord::Schema[7.1].define(version: 2021_04_28_120407) do
     t.datetime "updated_at", null: false
     t.string "submission_id"
   end
-
 end
